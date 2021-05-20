0c9890f64568fefdf99287010c5037c5412e5019
Luis Silva <luis.silva.prudencio@gmail.com>
2021-05-20 15:53:54 -0500

    first commit

---
+++ app/js/scripts.js
@@ -0,0 +1,23 @@
+const form = document.getElementById('form')
+const button = document.getElementById('button')
+const input = document.getElementById('input')
+const output = document.getElementById('output')
+
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    let total = calcularImpuesto(parseFloat(input.value))
+    let respuesta = total.toString()
+    let index = respuesta.indexOf('.')
+    if(respuesta==0){
+        output.textContent = `Resultado: $0.00`
+    }
+    if(index>0){
+        output.textContent = `Resultado: $${respuesta.slice(0, index+3)}`
+    }
+})
+
+const calcularImpuesto = (num) =>{
+    suma = num + num*0.64
+    return suma
+}
