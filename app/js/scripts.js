97f8af8f1374b07edf7d4c99e6efa555f1de270d
LuisStiveSilva <luis.silva.prudencio@gmail.com>
2022-07-14 12:10:36 -0500

    impuesto actualizado

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,25 +1,24 @@
-const form = document.getElementById('form')
-const button = document.getElementById('button')
-const input = document.getElementById('input')
-const output = document.getElementById('output')
+const form = document.getElementById("form");
+const button = document.getElementById("button");
+const input = document.getElementById("input");
+const output = document.getElementById("output");
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let total = calcularImpuesto(parseFloat(input.value));
+  let respuesta = total.toString();
+  let index = respuesta.indexOf(".");
+  if (respuesta == 0) {
+    output.textContent = `Resultado: $0.00`;
+  }
+  if (index > 0) {
+    output.textContent = `Resultado: $${respuesta.slice(0, index + 3)}`;
+  } else {
+    output.textContent = `Resultado: $${respuesta}`;
+  }
+});
 
-form.addEventListener('submit', (e)=>{
-    e.preventDefault()
-    let total = calcularImpuesto(parseFloat(input.value))
-    let respuesta = total.toString()
-    let index = respuesta.indexOf('.')
-    if(respuesta==0){
-        output.textContent = `Resultado: $0.00`
-    }
-    if(index>0){
-        output.textContent = `Resultado: $${respuesta.slice(0, index+3)}`
-    } else {
-        output.textContent = `Resultado: $${respuesta}`
-    }
-})
-
-const calcularImpuesto = (num) =>{
-    suma = num + num*0.64
-    return suma
-}
+const calcularImpuesto = (num) => {
+  suma = num + num * 0.75;
+  return suma;
+};
